9531d346a0207147c6af40b1f09c3305c91d2591
Ansel Rognlie <ansel@adadevelopersacademy.org>
2025-12-09 10:37:50 -0800

    updates formatting

--- app/models/greeting.py
+++ app/models/greeting.py
@@ -2,14 +2,15 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 from ..db import db
 
+
 class Greeting(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     greeting_text: Mapped[str]
-    character_id: Mapped[int] = mapped_column(ForeignKey("character.id")) 
+    character_id: Mapped[int] = mapped_column(ForeignKey("character.id"))
     character: Mapped["Character"] = relationship(back_populates="greetings")
 
-def to_dict(self):
+    def to_dict(self):
         return {
-            "id" : self.id,
-            "greeting" : self.greeting_text,
+            "id": self.id,
+            "greeting": self.greeting_text,
         }
